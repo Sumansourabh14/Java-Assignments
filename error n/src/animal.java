779c5e99a55fb48d564ab65e544187b5306594e6
Suman Sourabh <sumsourabh14@gmail.com>
2021-01-21 10:17:13 +0530

    Assignment 1 of Java OOPS

---
+++ error n/src/animal.java
@@ -0,0 +1,19 @@
+
+public class animal {
+	int age;
+	String n;
+
+	public animal() {
+		
+	}
+	
+	public void walk()
+	{
+		System.out.println(age);
+	}
+	public void speak()
+	{
+		System.out.println(n);
+	}
+
+}
---
+++ error n/src/earth.java
@@ -0,0 +1,15 @@
+
+public class earth {
+	
+	public static void main(String args[]) {
+		animal x;
+		x = new animal();
+		
+		x.age=5;
+		x.n="Horse";
+		
+		x.walk();
+		x.speak();
+	}
+
+}
